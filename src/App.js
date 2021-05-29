f58ecf6f965aeae893dcf1500a08316af30d2b9a
berriosa13 <berriosa13@gmail.com>
2021-05-29 09:16:08 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
-
 import image from './images/image.png';
 
 class App extends React.Component {
